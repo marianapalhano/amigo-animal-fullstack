872f026ae95e2f9e476beb88fa86faeb28694622
Mariana Palhano <36412023+marianapalhano@users.noreply.github.com>
2021-12-13 20:22:58 -0300

    Update config.ts

--- server/src/database/config.ts
+++ server/src/database/config.ts
@@ -8,7 +8,7 @@ export const connection = createConnection({
     host: "ec2-52-201-72-91.compute-1.amazonaws.com",
     port: 5432,
     username: "vuudbwysozmuzj",
-    password: "c9c3707fe676a5387944ef6731a92a350b17cb229603180449ea8e285eb08972",
+    password: "",
     database: "d2cm98n34b3lgo",
     logging: true,
     ssl: {
